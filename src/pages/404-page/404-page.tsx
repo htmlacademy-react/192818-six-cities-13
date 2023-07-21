e896eb065709db0f9d2cb3e5342cfedb95c23ffc
Кольчевский Павел Сергеевич (4080517) <pkolchevskiy@t1-consulting.ru>
2023-07-21 22:16:51 +0300

    fix: lint fixes

--- src/pages/404-page/404-page.tsx
+++ src/pages/404-page/404-page.tsx
@@ -1,10 +1,8 @@
 import { Link } from 'react-router-dom';
 
-export const NotFoundPage = () => {
-  return (
-    <div>
-      <h1>404 Not Found</h1>
-      <Link to="/">Go to main page</Link>
-    </div>
-  );
-};
+export const NotFoundPage = () => (
+  <div>
+    <h1>404 Not Found</h1>
+    <Link to="/">Go to main page</Link>
+  </div>
+);
